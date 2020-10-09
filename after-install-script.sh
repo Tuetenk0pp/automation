260e129f66cc438e01aef339ee5f0c88ef4dc199
Tuetenk0pp <j.vormschlag@protonmail.com>
2020-10-09 17:50:01 +0200

    minor improvements

--- after-install-script.sh
+++ after-install-script.sh
@@ -11,13 +11,18 @@ sudo apt-get install snapd -yy
 #add ppa's
 
 #nextcloud client
-sudo apt-add-repository ppa:nextcloud-devs/client -no-update
+sudo apt-add-repository ppa:nextcloud-devs/client -yy -no-update
 
 #atom text editor
 wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-sudo apt-add-repository https://cli.github.com/packages --no-update
+
+#github command line interface
+sudo apt-add-repository https://cli.github.com/packages -yy --no-update
+
+#keepassxc-latest
+sudo add-apt-repository ppa:phoerious/keepassxc -yy --no-update
 
 
 #install software
@@ -25,7 +30,7 @@ sudo apt-add-repository https://cli.github.com/packages --no-update
 sudo apt-get update
 sudo apt-get install firefox nautilus-nextcloud atom thunderbird telegram-desktop \
 gh steam grub-customizer gnome-tweaks libreoffice-calc libreoffice-writer libreoffice-gnome \
-libreoffice-impress libreoffice-math libreoffice-l10n-de -yy
+libreoffice-impress libreoffice-math libreoffice-l10n-de keepassxc -yy
 
 #install obs: uncommend if desired
 #sudo apt install ffmpeg
@@ -34,6 +39,6 @@ libreoffice-impress libreoffice-math libreoffice-l10n-de -yy
 
 #snap
 sudo snap refresh
-sudo snap install keepassxc discord
+sudo snap install discord
 
 #configure software
