72802dc0224bbaa6893e6f2b3fc3e0ec30cecfb9
Tuetenk0pp <55783419+Tuetenk0pp@users.noreply.github.com>
2020-11-18 21:12:57 +0100

    changes for pop os

--- ubuntu-after-install.sh
+++ ubuntu-after-install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 #update everything
-sudo apt-get update && sudo apt-get upgrade -yy
-sudo apt-get install snapd -yy
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install snapd -y
 
-#remove software
+#remove unwanted software
 #sudo apt purge somepackage
 
 #add ppa's
@@ -18,26 +18,33 @@ sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
 
 #github command line interface
-sudo apt-add-repository https://cli.github.com/packages -y --no-update
+sudo apt-add-repository https://cli.github.com/packages -y
 
 #keepassxc-latest
 sudo add-apt-repository ppa:phoerious/keepassxc -y --no-update
 
 #gamehub-latest
-sudo add-apt-repository ppa:tkashkin/gamehub -y -no-update
+sudo add-apt-repository ppa:tkashkin/gamehub -y --no-update
+
+#OBS
+sudo add-apt-repository ppa:obsproject/obs-studio -y --no-update
 
 #pop_os! themes
-sudo add-apt-repository ppa:system76/pop
+#sudo add-apt-repository ppa:system76/pop -y --no-update
 
 #install software
+
 #apt
 sudo apt-get update
-sudo apt-get install firefox nautilus-nextcloud atom thunderbird telegram-desktop gh steam grub-customizer gnome-tweaks gnome-extensions libreoffice-calc libreoffice-writer libreoffice-gnome libreoffice-impress libreoffice-math libreoffice-l10n-de keepassxc com.github.tkashkin.gamehub pop-theme -yy
+sudo apt-get install firefox nautilus-nextcloud atom telegram-desktop steam gnome-tweaks gnome-extensions keepassxc -y #basics
+#sudo apt-get install com.github.tkashkin.gamehub -y #gamehub
+#sudo apt-get install gh -y #github cli
+#sudo apt-get install libreoffice-calc libreoffice-writer libreoffice-gnome libreoffice-impress libreoffice-math libreoffice-l10n-de -y #office
+sudo apt-get install texlive texlive-lang-german texlive-latex-extra -y #LateX
 
 #install obs: uncommend if desired
-#sudo apt install ffmpeg
-#sudo add-apt-repository ppa:obsproject/obs-studio
-#sudo apt install obs-studio
+#sudo apt-get install ffmpeg -y
+#sudo apt-get install obs-studio -y
 
 #snap
 sudo snap refresh
@@ -48,31 +55,34 @@ sudo snap install discord
 #theming
 
 #apply wallpaper
-cp ./automation/wallpaper.png /usr/share/backgrounds/Tuetenk0pp-automation-wallpaper.png
-gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/Tuetenk0pp-automation-wallpaper.png
+#cp ./automation/wallpaper.png /usr/share/backgrounds/Tuetenk0pp-automation-wallpaper.png
+#gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/Tuetenk0pp-automation-wallpaper.png
 
 #install and apply grub theme
-git clone https://github.com/vinceliuice/grub2-themes.git
-cd grub2-themes/
-sudo ./install.sh --vimix --boot
-cd
-sudo rm -r grub-themes/
-grub-customizer
-read -p "Are you done? Press [ENTER] to continue"
+#git clone https://github.com/vinceliuice/grub2-themes.git
+#cd grub2-themes/
+#sudo ./install.sh --vimix --boot
+#cd
+#sudo rm -r grub-themes/
+#grub-customizer &
+#read -p "Are you done? Press [ENTER] to continue"
 
 #configure firefox
 cat ./automation/firefox.txt
-firefox
+firefox &
 read -p "Are you done? Press [ENTER] to continue"
 
 #configure gnome-extensions
-gnome-extensions disable ubuntu-dock@ubuntu.com
-gnome-extensions enable dash-to-dock@micxgx.gmail.com user-theme@gnome-shell-extensions.gcampax.github.com
-gnome-extensions prefs dash-to-dock@micxgx.gmail.com
-read -p "Are you done? Press [ENTER] to continue"
+#gnome-extensions disable ubuntu-dock@ubuntu.com
+#gnome-extensions enable dash-to-dock@micxgx.gmail.com user-theme@gnome-shell-extensions.gcampax.github.com
+#gnome-extensions prefs dash-to-dock@micxgx.gmail.com
+#read -p "Are you done? Press [ENTER] to continue"
 
 #apply pop_os! themes
-gnome-tweaks
-read -p "Are you done? Press [ENTER] to continue"
+#sudo apt-get install pop-theme
+#gnome-tweaks
+#read -p "Are you done? Press [ENTER] to continue"
+
+sudo apt autoremove -y
 
 cat ./automation/final.txt
